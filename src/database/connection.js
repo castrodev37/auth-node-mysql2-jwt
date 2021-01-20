450d09c0e40c8ab0b996f52719283736a8f56722
Cayo Augusto <devcastro37@gmail.com>
2021-01-20 01:55:05 -0300

    initial express server

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 module.exports = async ()=>{
   if(global.connection && global.connection.state !== 'disconnected')
     return global.connection
 
   const mysql = require('mysql2/promise')
   
-  const connection = await mysql.createConnection(`mysql://root@localhost:3306/nodemysql`)
+  const connection = await mysql.createConnection(`mysql://${DB_USER}@${DB_HOST}:${DB_PORT}/${DB_NAME}`)
   
   console.log('DB connected!');
   
